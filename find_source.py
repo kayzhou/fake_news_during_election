189462fde233128f16dd3521291aa7e466bd1b4b
Kay Zhou <zkzhou_91@163.com>
2018-11-01 17:09:36 -0400

    update

--- find_source.py
+++ find_source.py
@@ -27,7 +27,6 @@ def find_retweets(tweets_ids, out_name):
     for _id in tweets_ids:
         q.add(_id)
 
-    dealed = set([])
     conn = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_db.sqlite")
     c = conn.cursor()
 
@@ -36,25 +35,18 @@ def find_retweets(tweets_ids, out_name):
     with open(out_name, "w") as f:
         while q:
             _id = q.pop()
-
             cnt += 1
             if cnt % 20000 == 0:
                 # print(_id, _id in dealed)
-                print('已经处理的点：', len(dealed), "；边的数量：", edge_cnt, "；等待处理队列：", len(q))
-            dealed.add(_id)
+                print("；边的数量：", edge_cnt, "；等待处理队列：", len(q))
 
             c.execute('''SELECT tweet_id FROM tweet_to_retweeted_uid WHERE retweet_id={};'''.format(_id))
-            data = c.fetchall()
 
-            for line in data:
+            for line in c.fetchall():
                 tid = line[0]
                 edge_cnt += 1
                 f.write("{}\t{}\n".format(_id, tid))
-                if tid not in dealed:
-                    new_nodes.add(tid)
-                    # print("终于添加新点了！", len(q))
-                    q.add(tid)
-    print(len(new_nodes))
+                q.add(tid)
 
 def load_all_nodes_v1():
     tweets_ids = set([])
