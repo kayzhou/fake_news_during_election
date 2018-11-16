aee3b43bd40f77874e4aeb90ab7f8ad616891438
Kay Zhou <zkzhou_91@163.com>
2018-11-16 09:52:08 -0500

    update

--- find_IRAs.py
+++ find_IRAs.py
@@ -6,84 +6,24 @@ Created on 2018-11-12 15:55:59
 """
 
 import pandas as pd
-import sqlite3
 import json
 from tqdm import tqdm
 
-def find_tweet(_id):
-    conn1 = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_db.sqlite")
-    conn2 = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_sep-nov_db.sqlite")
-    c1 = conn1.cursor()
-    c2 = conn2.cursor()
 
-    new_d = {}
-    c1.execute('''SELECT * FROM tweet WHERE tweet_id={}'''.format(_id))
-    d = c1.fetchone()
-    if d:
-        col_name = [t[0] for t in c1.description]
-            # print(d)
-        for k, v in zip(col_name, d):
-            new_d[k] = v
+found_IRAs = {}
 
-    else:
-        c2.execute('''SELECT * FROM tweet WHERE tweet_id={}'''.format(_id))
-        d = c2.fetchone()
-        if d:
-            col_name = [t[0] for t in c1.description]
-            # print(d)
-            for k, v in zip(col_name, d):
-                new_d[k] = v
-
-    conn1.close()
-    conn2.close()
-
-    if not new_d:
-        new_d = find_retweeted(_id)
-
-    # if not new_d:
-    #     print("找不到该tweet：", _id)
-
-    return new_d
-
-
-def find_retweeted(_id):
-    conn1 = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_db.sqlite")
-    conn2 = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_sep-nov_db.sqlite")
-    c1 = conn1.cursor()
-    c2 = conn2.cursor()
-
-    new_d = {}
-    c1.execute('''SELECT * FROM retweeted_status WHERE tweet_id={}'''.format(_id))
-    d = c1.fetchone()
-    if d:
-        col_name = [t[0] for t in c1.description]
-            # print(d)
-        for k, v in zip(col_name, d):
-            new_d[k] = v
-
-    else:
-        c2.execute('''SELECT * FROM retweeted_status WHERE tweet_id={}'''.format(_id))
-        d = c2.fetchone()
-        if d:
-            col_name = [t[0] for t in c1.description]
-            # print(d)
-            for k, v in zip(col_name, d):
-                new_d[k] = v
-        # else:
-        #     print("没有找到原创tweet (retweet)", _id)
-
-    conn1.close()
-    conn2.close()
-
-    return new_d
-
+for line in open("data/IRAs_be_found.json"):
+    d = json.loads(line.strip())
+    found_IRAs[d["tweet_id"]] = d["user_id"]
 
 data = pd.read_csv("data/ira_tweets_csv_hashed.csv", usecols=["tweetid", "userid"])
 
-with open("data/IRAs_be_found.json", "w") as f:
+with open("data/users_match.txt", "w") as f:
     for i, row in tqdm(data.iterrows()):
         tid = row["tweetid"]
-        tweet = find_tweet(tid)
-        if tweet:
-            # tweet["bingo"] = True
-            f.write(json.dumps(tweet, ensure_ascii=False) + "\n")
+        uid = row["userid"]
+        if tid in found_IRAs:
+            real_uid = found_IRAs[tid]
+            if real_uid != uid:
+                f.write("{}\t{}\n".format(uid, real_uid))
+
