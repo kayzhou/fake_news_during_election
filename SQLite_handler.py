2637043404fbf322651f9d6243bb75309b3ef65b
Alexandre Bovet <abovet@ccny.cuny.edu>
2018-11-16 11:16:12 -0500

    update

--- SQLite_handler.py
+++ SQLite_handler.py
@@ -76,6 +76,38 @@ def find_retweeted(_id):
     return new_d
 
 
+def find_original_tweetid(_id):
+    conn1 = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_db.sqlite")
+    conn2 = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_sep-nov_db.sqlite")
+    c1 = conn1.cursor()
+    c2 = conn2.cursor()
+
+    new_d = {}
+    c1.execute('''SELECT * FROM tweet_to_retweeted_uid WHERE tweet_id={}'''.format(_id))
+    d = c1.fetchone()
+    if d:
+        col_name = [t[0] for t in c1.description]
+            # print(d)
+        for k, v in zip(col_name, d):
+            new_d[k] = v
+
+    else:
+        c2.execute('''SELECT * FROM tweet_to_retweeted_uid WHERE tweet_id={}'''.format(_id))
+        d = c2.fetchone()
+        if d:
+            col_name = [t[0] for t in c2.description]
+            # print(d)
+            for k, v in zip(col_name, d):
+                new_d[k] = v
+        else:
+            print("没有转发关系！", _id)
+            
+    conn1.close()
+    conn2.close()
+
+    return new_d 
+
+
 def get_hashtag_tweet_user():
     conn1 = sqlite3.connect("/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_db.sqlite")
     c1 = conn1.cursor()
