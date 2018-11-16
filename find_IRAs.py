a9365e167c603d4b3c574296ec93f0946c46bd01
Kay Zhou <zkzhou_91@163.com>
2018-11-16 09:54:05 -0500

    update

--- find_IRAs.py
+++ find_IRAs.py
@@ -9,6 +9,29 @@ import pandas as pd
 import json
 from tqdm import tqdm
 
+"""
+# 找出真实的user_id
+count = 0
+with open("data/IRAs_be_found.json", "w") as f:
+    for i, row in tqdm(data.iterrows()):
+        tid = row["tweetid"]
+        tweet = find_tweet(tid)
+        if tweet:
+            count += 1
+            if count % 1000 == 0:
+                print(count)
+            f.write(json.dumps(tweet, ensure_ascii=False) + "\n")
+
+# 统计有多少个匿名用户
+data = pd.read_csv("data/ira_users_csv_hashed.csv")
+count = 0
+for i, row in tqdm(data.iterrows()):
+    uid = row["userid"]
+    if len(uid) == len("b76861a43db67db9e6c51db1d37d1a622bb7a8fc14a17c880f10f7d1f16a7175"):
+        count += 1
+
+print(count)
+"""
 
 found_IRAs = {}
 
