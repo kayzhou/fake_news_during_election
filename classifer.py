33fda76d4b5136a13ed1f8884d4cf10359b1afdf
Kay Zhou <zkzhou_91@163.com>
2018-12-18 14:00:51 -0500

    update

--- classifer.py
+++ classifer.py
@@ -150,7 +150,17 @@ savedir = '/home/alex/network_workdir/elections/tweet_classification/trump_vs_hi
 
 
 #%%
+def is_discuss_trump_hillary(row):
+    words = set(str(row["tweet_text"]).lower().strip().split())
 
+    if "trump" in words or "realdonaldtrump" in words or "@realdonaldtrump" in words or "donaldtrump" in words:
+        return True
+    elif "hillary" in words or "clinton" in words or "hillaryclinton" in words or "@hillaryclinton" in words:
+        return True
+    else:
+        return False
+
+savedir = '/home/alex/network_workdir/elections/tweet_classification/trump_vs_hillary_june_sep_signi/trained_classifier/'
 filename = 'sklearn_SGDLogReg_' + suffix + '.pickle'
 
 print('loading ' + savedir + filename)
@@ -158,14 +168,35 @@ cls = joblib.load(savedir + filename)
 
 classifier = cls['sklearn_pipeline']
 label_inv_mapper = cls['label_inv_mapper']
-
 TweetClass = TweetClassifier(classifier=classifier, label_inv_mapper=label_inv_mapper)
 
 tweets = pd.read_csv('data/ira_tweets_csv_hashed.csv', low_memory=False)
-tweets = tweets[tweets["tweet_time"] >= "2016-09-01 00:00"][tweets["tweet_time"] < "2016-11-09 00:00"]
+tws = tweets[tweets["tweet_time"] >= "2016-06-01 00:00"][tweets["tweet_time"] < "2016-09-01 00:00"]
+
+with open("data/IRA-pro-trump.txt", "a") as f:
+    for i, row in tqdm(tws.iterrows()):
+        if is_discuss_trump_hillary(row):
+            line = row["tweet_text"]
+            predict_proba = TweetClass.classify_text(line, return_pred_labels=False)
+            f.write("{},{}\n".format(row["tweetid"], predict_proba[0]))
+
+
+savedir = '/home/alex/network_workdir/elections/tweet_classification/trump_vs_hillary_sep-nov/trained_classifier/'
+filename = 'sklearn_SGDLogReg_' + suffix + '.pickle'
+
+print('loading ' + savedir + filename)
+cls = joblib.load(savedir + filename)
+
+classifier = cls['sklearn_pipeline']
+label_inv_mapper = cls['label_inv_mapper']
+TweetClass = TweetClassifier(classifier=classifier, label_inv_mapper=label_inv_mapper)
+
+# tweets = pd.read_csv('data/ira_tweets_csv_hashed.csv', low_memory=False)
+tws = tweets[tweets["tweet_time"] >= "2016-09-01 00:00"][tweets["tweet_time"] < "2016-11-09 00:00"]
 
 with open("data/IRA-pro-trump.txt", "a") as f:
     for i, row in tqdm(tweets.iterrows()):
-        line = row["tweet_text"]
-        predict_proba = TweetClass.classify_text(line, return_pred_labels=False)
-        f.write("{},{}\n".format(row["tweetid"], predict_proba[0]))
+        if is_discuss_trump_hillary(tws):
+            line = row["tweet_text"]
+            predict_proba = TweetClass.classify_text(line, return_pred_labels=False)
+            f.write("{},{}\n".format(row["tweetid"], predict_proba[0]))
