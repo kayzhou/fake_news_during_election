d84972a4ac63bc2f27fef2cf33b08882ed78b0aa
Kay Zhou <zkzhou_91@163.com>
2018-12-26 15:26:44 -0500

    update

--- IRAs_overlap.py
+++ IRAs_overlap.py
@@ -68,7 +68,7 @@ class analyze_IRA_in_network:
 
     def run(self):
         # self.find_user_id_map()
-        self.check()
+        self.get_map()
 
 if __name__ == "__main__":
     Lebron = analyze_IRA_in_network()
