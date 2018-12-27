48f90b8efd939e35a1442cf34b1298eac9bb5fd0
Kay Zhou <zkzhou_91@163.com>
2018-12-27 18:09:25 -0500

    update

--- fake_identify.py
+++ fake_identify.py
@@ -1,29 +1,32 @@
-import ujson as json
+import json
 
-host_label = json.load(open("data/source.txt"))
-host_label = json.load(open("data/mbfc_host_label.json"))
-# d = json.load(open('data/sources.json'))
-# for k, v in d.items():
-#     if v['type'] in ['fake', 'conspiracy', 'hate', 'Conspiracy', 'fake news']:
-#         print(k, '-2', sep='\t')
-#     if v['type'] == 'bias':
-#         print(k, '-1', sep='\t')
+NEW_HOST_1 = {}
+for k, v in json.load(open("data/sources.json")).items():
+    hostname = k.lower()
+    _type = v["type"]
+    if _type in ["fake", "conspiracy", "hate"]:
+        NEW_HOST_1[hostname] = "FAKE"
+    elif _type == "bias":
+        NEW_HOST_1[hostname] = "BIAS"
 
-# dict_labels = {}
-# for line in open('data/hostname_label.txt'):
-#     host, label = line.strip().split('\t')
-#     dict_labels[host] = label
-# print(len(dict_labels))
-# json.dump(dict_labels, open('data/host_label.json', 'w'), indent=4)
+NEW_HOST_2 = {k.lower(): v for k, v in json.load(open("data/mbfc_host_label.json")).items()}
 
-def how_the_news(host_name):
-    '''
-    -2  fake
-    -1  bias
-    0   left
-    1   leaning left
-    2   center
-    3   leaning right
-    4   right
-    '''
-    return host_label[host_name]
+
+def kind_of_news(ht):
+    ht = ht.lower()
+    labels = []
+    if ht in NEW_HOST_1:
+        labels.append(NEW_HOST_1[ht])
+    else:
+        labels.append("NOT FAKE")
+
+    if ht in NEW_HOST_2:
+        labels.extend(NEW_HOST_2[ht])
+    else:
+        labels.extend([-1, -1])
+
+    return labels
+
+
+if __name__ == "__main__":
+    print(kind_of_news("cnn.com"))
