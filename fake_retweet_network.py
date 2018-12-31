cdd2239cced1160da31d2479bc1f6154df5b2244
Kay Zhou <zkzhou_91@163.com>
2018-12-31 15:32:03 -0500

    update

--- fake_retweet_network.py
+++ fake_retweet_network.py
@@ -137,7 +137,7 @@ def find_links(tweets_ids):
 
 def find_fake_news_sources():
     data = json.load(open("data/fake_retweet_network.json"))
-    not_sources = str([str(v) for v in data.keys()])
+    not_sources = set([str(v) for v in data.keys()])
 
     t_ids = set([str(json.loads(line.strip())["tweet_id"]) for line in open("data/fake_tweets.json")])
     print(len(t_ids))
@@ -255,17 +255,17 @@ def get_tweets(tweets_ids):
 if __name__ == "__main__":
 
     # 找出所有fake_news
-    find_fake_tweets()
+    # find_fake_tweets()
 
     # 获取转发关系
-    t_ids = set([str(json.loads(line.strip())["tweet_id"]) for line in open("data/fake_tweets.json")])
-    print(len(t_ids))
-    ira_t_ids = set([str(json.loads(line.strip())["tweetid"]) for line in open("data/IRA_fake_tweets.json")])
-    print(len(ira_t_ids))
-    t_ids = t_ids | ira_t_ids
-    print(len(t_ids))
+    # t_ids = set([str(json.loads(line.strip())["tweet_id"]) for line in open("data/fake_tweets.json")])
+    # print(len(t_ids))
+    # ira_t_ids = set([str(json.loads(line.strip())["tweetid"]) for line in open("data/IRA_fake_tweets.json")])
+    # print(len(ira_t_ids))
+    # t_ids = t_ids | ira_t_ids
+    # print(len(t_ids))
+    # find_links(t_ids)
 
-    find_links(t_ids)
     find_fake_news_sources()
 
     # tids = load_fake_news_source()
