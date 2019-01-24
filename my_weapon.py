bb1affd778ed9fed07549d9626747b282bf030c2
Kay Zhou <zkzhou_91@163.com>
2019-01-24 16:47:02 -0500

    update

--- my_weapon.py
+++ my_weapon.py
@@ -7,12 +7,13 @@ Created on 2018-11-19 14:45:24
 
 import json
 
+import networkx as nx
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
 try:
     import matplotlib.pyplot as plt
     import seaborn as sns
 except:
     print("import plt error.")
-import networkx as nx
-import numpy as np
-import pandas as pd
-from tqdm import tqdm
