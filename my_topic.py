027d6f9ebff78699a9ee3e1377bdc17a482de29d
Kay Zhou <zkzhou_91@163.com>
2019-01-27 18:23:38 -0500

    update

--- my_topic.py
+++ my_topic.py
@@ -9,6 +9,7 @@ import sqlite3
 
 from gensim.models import LdaModel
 from gensim.corpora import Dictionary
+from gensim.test.utils import datapath
 from tqdm import tqdm
 
 from Trump_Clinton_Classifer.TwProcess import CustomTweetTokenizer
@@ -26,15 +27,27 @@ c = conn.cursor()
 c.execute('''SELECT text FROM tweet LIMIT 100''')
 texts = [tokenizer.tokenize(d[0]) for d in c.fetchall()]
 dictionary = Dictionary(texts)
+# print(texts[0])
+# print(dictionary.doc2bow(texts[0]))
+# print(lda[dictionary.doc2bow(texts[0])])
+conn.close()
+
+conn = sqlite3.connect(
+    "/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_sep-nov_db.sqlite")
+c = conn.cursor()
+c.execute('''SELECT text FROM tweet LIMIT 100''')
+texts = [tokenizer.tokenize(d[0]) for d in c.fetchall()]
+dictionary.add_documents(texts)
+conn.close()
 
 corpus = [dictionary.doc2bow(t) for t in texts]
 lda = LdaModel(corpus, num_topics=10)
 
-print(texts[0])
-print(dictionary.doc2bow(texts[0]))
-print(lda[dictionary.doc2bow(texts[0])])
-conn.close()
+# print(texts[0])
+# print(dictionary.doc2bow(texts[0]))
+# print(lda[dictionary.doc2bow(texts[0])])
 
-# conn = sqlite3.connect(
-#     "/home/alex/network_workdir/elections/databases_ssd/complete_trump_vs_hillary_sep-nov_db.sqlite")
+temp_file = datapath("model/topic.model")
+lda.save(temp_file)
+
 
