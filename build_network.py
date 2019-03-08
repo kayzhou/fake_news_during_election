629b1b139b8e90c849cdd46fe6314b03252393d8
Kay Zhou <zkzhou_91@163.com>
2019-03-08 18:35:12 -0500

    update

--- build_network.py
+++ build_network.py
@@ -591,7 +591,7 @@ def build_networks_within_ira():
     for in_name in tqdm(in_files):
         for line in open(in_name):
             d = json.loads(line)
-            print(d["created_at"], type(d["created_at"]))
+            print(d["created_at"], type(d["created_at"]), d)
             dt = pendulum.parse(d["created_at"]).int_timestamp
             if dt > end or dt < start:
                 continue
